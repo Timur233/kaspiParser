d3c2577e32a374f587f7d0b06825402b4c02be86
Iskandarov Timur <timur.iskandarov96@gmail.com>
2023-05-24 18:09:15 +0600

    New Setup

--- parser-for-you.js
+++ parser-for-you.js
@@ -7,7 +7,7 @@ const config = {
     isKaspiUpdate: true,
     cabinetWindow: null,
     marketWindow: null,
-    myMarckets: ['Power Steel', 'Intexmania-kz'],
+    myMarckets: ['Power Steel', 'Intexmania-kz', 'Aquaintex-asia-kz'],
     screen: {
         width: 1920,
         height: 1080
