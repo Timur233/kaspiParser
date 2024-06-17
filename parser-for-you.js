c609a2c0426fd645db634f8dc757c4ca2946e12d
Iskandarov Timur <timur.iskandarov96@gmail.com>
2024-06-17 23:26:51 +0500

    surprise

--- parser-for-you.js
+++ parser-for-you.js
@@ -282,7 +282,7 @@ const config = {
     async function getOptimalPrice(id, sku, productMinPrice, maxPrice, sallerTable) {
 
         let optimalPrice = productMinPrice;
-        const minPrice = productMinPrice - 11;
+        const minPrice = productMinPrice - calculateDiscount(productMinPrice);
         let sallerName = sallerTable[0].saller;
         let sallerPrice = sallerTable[0].price;
 
@@ -306,7 +306,7 @@ const config = {
         for (let offer of sallerTable) {
             if (minPrice < offer.price && !config.myMarckets.includes(offer.saller)
                 ) {
-                    optimalPrice = offer.price - 11;
+                    optimalPrice = offer.price - calculateDiscount(offer.price);
                     sallerName = offer.saller;
                     sallerPrice = offer.price;
 
