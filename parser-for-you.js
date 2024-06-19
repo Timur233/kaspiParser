54f0d34a74bea3ea684a8049627e16ce5c83a064
Iskandarov Timur <timur.iskandarov96@gmail.com>
2024-06-19 10:25:35 +0500

    Bestway asia

--- parser-for-you.js
+++ parser-for-you.js
@@ -7,7 +7,7 @@ const config = {
     isKaspiUpdate: true,
     cabinetWindow: null,
     marketWindow: null,
-    myMarckets: ['Power Steel', 'Intexmania-kz', 'Aquaintex-asia-kz', 'ИП BESTWAY ASIA'],
+    myMarckets: ['Power Steel', 'Intexmania-kz', 'Aquaintex-asia-kz', 'BESTWAY ASIA'],
     screen: {
         width: 1920,
         height: 1080
