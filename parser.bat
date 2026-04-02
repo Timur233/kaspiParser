7948ef1d7aa6ff7118061815c1f51556c87c617f
iskandarov <iskandarov@kolesa.kz>
2026-04-03 02:08:50 +0500

    Hide Windows parser launchers

--- parser.bat
+++ parser.bat
@@ -1,4 +1,12 @@
 @echo off
+setlocal
+
+if /i "%~1"=="__hidden__" goto run
+
+mshta "vbscript:CreateObject(""WScript.Shell"").Run """"%~f0"""" __hidden__"", 0, False:close"
+exit /b
+
+:run
 cd /d "%~dp0"
 node scripts\run-parser.js for-you
-exit
+exit /b
